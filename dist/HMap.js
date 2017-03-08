341eae7be9fc669fb5413893aae7f1575b3cdae7
FDD <smileFDD@gmail.com>
2017-03-08 22:31:55 +0800

    多继承

--- dist/HMap.js
+++ dist/HMap.js
@@ -149,13 +149,32 @@ Object.defineProperty(exports, "__esModule", {
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
+var _mixin = __webpack_require__(6);
+
+var _mixin2 = _interopRequireDefault(_mixin);
+
+var _Style = __webpack_require__(7);
+
+var _Style2 = _interopRequireDefault(_Style);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var Layer = function () {
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var Layer = function (_mix) {
+  _inherits(Layer, _mix);
+
   function Layer() {
     _classCallCheck(this, Layer);
 
-    this.model = 'Layer';
+    var _this = _possibleConstructorReturn(this, (Layer.__proto__ || Object.getPrototypeOf(Layer)).call(this));
+
+    _this.model = 'Layer';
+    return _this;
   }
 
   _createClass(Layer, [{
@@ -163,10 +182,16 @@ var Layer = function () {
     value: function getModel() {
       return this.model;
     }
+  }, {
+    key: 'setStyle',
+    value: function setStyle() {
+      this.style = this.getStyle();
+      return this.style;
+    }
   }]);
 
   return Layer;
-}();
+}((0, _mixin2.default)(_Style2.default));
 
 exports.default = Layer;
 module.exports = exports['default'];
@@ -277,29 +302,191 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
-var _mapConfig = __webpack_require__(0);
-
-var _mapConfig2 = _interopRequireDefault(_mapConfig);
-
-var _Map = __webpack_require__(2);
-
-var _Map2 = _interopRequireDefault(_Map);
-
-var _Layer = __webpack_require__(1);
-
-var _Layer2 = _interopRequireDefault(_Layer);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+var _constants = __webpack_require__(5);
 
 var HMap = {};
 
 HMap.version = __webpack_require__(3).version;
 
-HMap.Map = _Map2.default;
-HMap.Layer = _Layer2.default;
-HMap.config = _mapConfig2.default;
+HMap.Map = _constants.Map;
+HMap.Layer = _constants.Layer;
+HMap.config = _constants.config;
 
 exports.default = HMap;
+module.exports = exports['default'];
+
+/***/ }),
+/* 5 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.mix = exports.config = exports.Style = exports.Map = exports.Layer = exports.ee = exports.a = exports.PI = exports.x_PI = undefined;
+
+var _mapConfig = __webpack_require__(0);
+
+var _mapConfig2 = _interopRequireDefault(_mapConfig);
+
+var _Map2 = __webpack_require__(2);
+
+var _Map3 = _interopRequireDefault(_Map2);
+
+var _Layer2 = __webpack_require__(1);
+
+var _Layer3 = _interopRequireDefault(_Layer2);
+
+var _Style2 = __webpack_require__(7);
+
+var _Style3 = _interopRequireDefault(_Style2);
+
+var _mixin = __webpack_require__(6);
+
+var _mixin2 = _interopRequireDefault(_mixin);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+/**
+ * Created by FDD on 2017/2/22.
+ * @desc 静态常量
+ */
+
+var x_PI = exports.x_PI = 3.14159265358979324 * 3000.0 / 180.0;
+var PI = exports.PI = 3.1415926535897932384626; // PI
+var a = exports.a = 6378245.0; // 北京54坐标系长半轴a=6378245m
+var ee = exports.ee = 0.00669342162296594323;
+
+// modules
+var Layer = exports.Layer = _Layer3.default;
+var Map = exports.Map = _Map3.default;
+var Style = exports.Style = _Style3.default;
+var config = exports.config = _mapConfig2.default;
+var mix = exports.mix = _mixin2.default;
+
+/***/ }),
+/* 6 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var mix = function mix() {
+  for (var _len = arguments.length, mixins = Array(_len), _key = 0; _key < _len; _key++) {
+    mixins[_key] = arguments[_key];
+  }
+
+  var Mix = function Mix() {
+    _classCallCheck(this, Mix);
+  };
+  // 以编程方式给Mix类添加
+
+  // mixins的所有方法和访问器
+
+
+  var _iteratorNormalCompletion = true;
+  var _didIteratorError = false;
+  var _iteratorError = undefined;
+
+  try {
+    for (var _iterator = mixins[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+      var mixin = _step.value;
+
+      copyProperties(Mix, mixin);
+      copyProperties(Mix.prototype, mixin.prototype);
+    }
+  } catch (err) {
+    _didIteratorError = true;
+    _iteratorError = err;
+  } finally {
+    try {
+      if (!_iteratorNormalCompletion && _iterator.return) {
+        _iterator.return();
+      }
+    } finally {
+      if (_didIteratorError) {
+        throw _iteratorError;
+      }
+    }
+  }
+
+  return Mix;
+};
+
+var copyProperties = function copyProperties(target, source) {
+  var _iteratorNormalCompletion2 = true;
+  var _didIteratorError2 = false;
+  var _iteratorError2 = undefined;
+
+  try {
+    for (var _iterator2 = Reflect.ownKeys(source)[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
+      var key = _step2.value;
+
+      if (key !== "constructor" && key !== "prototype" && key !== "name") {
+        var desc = Object.getOwnPropertyDescriptor(source, key);
+        Object.defineProperty(target, key, desc);
+      }
+    }
+  } catch (err) {
+    _didIteratorError2 = true;
+    _iteratorError2 = err;
+  } finally {
+    try {
+      if (!_iteratorNormalCompletion2 && _iterator2.return) {
+        _iterator2.return();
+      }
+    } finally {
+      if (_didIteratorError2) {
+        throw _iteratorError2;
+      }
+    }
+  }
+};
+
+exports.default = mix;
+module.exports = exports['default'];
+
+/***/ }),
+/* 7 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Style = function () {
+  function Style() {
+    _classCallCheck(this, Style);
+
+    this.model = 'Style';
+  }
+
+  _createClass(Style, [{
+    key: 'getStyle',
+    value: function getStyle() {
+      return this.model;
+    }
+  }]);
+
+  return Style;
+}();
+
+exports.default = Style;
 module.exports = exports['default'];
 
 /***/ })
