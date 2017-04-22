aae55992b0477486304163617ff8d438b8d1bb10
FDD <smileFDD@gmail.com>
2017-04-22 11:47:46 +0800

    //暂时去除proj4，引用报错

--- src/constants.js
+++ src/constants.js
@@ -8,7 +8,7 @@ export const PI = 3.1415926535897932384626 // PI
 export const a = 6378245.0 // 北京54坐标系长半轴a=6378245m
 export const ee = 0.00669342162296594323
 
-import _proj4 from '../node_modules/proj4'
+// import _proj4 from '../node_modules/proj4'
 import _ol from '../node_modules/openlayers'
 import _mathjs from '../node_modules/mathjs'
 // import _olx from '../node_modules/openlayers/externs'
@@ -17,7 +17,7 @@ import _mathjs from '../node_modules/mathjs'
 import _config from './config/config'
 
 export const ol = _ol
-export const proj4 = _proj4
+// export const proj4 = _proj4
 // export const olx = _olx;
 export const config = _config
 export const math = _mathjs
