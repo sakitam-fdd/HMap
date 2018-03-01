26a5fb0c0143ab1f724fae3f1846440418df14cb
sakitam-fdd <smileFDD@gmail.com>
2018-03-01 18:14:54 +0800

    edit

--- build/rollup-dev-config.js
+++ build/rollup-dev-config.js
@@ -6,13 +6,15 @@ module.exports = Object.assign({
   plugins: [
     serve({
       open: true, // 是否打开浏览器
-      contentBase: ['dist', 'example'],
+      contentBase: ['dist', 'example', 'docs'],
       historyApiFallback: true, // Set to true to return index.html instead of 404
       host: '127.0.0.1',
       port: 3333,
     }),
     livereload({
-      watch: ['dist', 'example']
+      watch: ['dist', 'example', 'docs']
     })
   ]
 }, input, { output })
+
+console.log(`examples: http://127.0.0.1:3333`)
