5ec5f72683778fc9b46ee02206e29908e019716b
sakitam-fdd <smilefdd@gmail.com>
2018-08-01 18:10:01 +0800

    add geoserver vector tile example

--- src/layer/Layer.js
+++ src/layer/Layer.js
@@ -379,6 +379,28 @@ class Layer {
    * @returns {string}
    */
   createTileWMSLayer (layerName, params) {
+    const _config = {
+      LAYERS: params['layers'], // require
+      STYLES: params['styles'] ? params['styles'] : '',
+      VERSION: params['version'] ? params['version'] : '1.3.0',
+      WIDTH: params['width'] ? params['width'] : 256,
+      HEIGHT: params['height'] ? params['height'] : 256,
+      BBOX: params['bbox'], // require
+      SRS: params['srs'] ? params['srs'] : 'EPSG:3857',
+      CRS: params['srs'] ? params['srs'] : 'EPSG:3857',
+      REQUEST: 'GetMap',
+      TRANSPARENT: true,
+      TILED: params['tiled'] === false ? params['tiled'] : true,
+      TILESORIGIN: params['tiledsorrigin']
+        ? params['tiledsorrigin']
+        : undefined,
+      SERVICE: 'WMS',
+      FORMAT: params['format'] ? params['format'] : 'image/png',
+      VIEWPARAMS: params['viewparams'] ? params['viewparams'] : ''
+    };
+    if (params['cql_filter']) {
+      _config.CQL_FILTER = params['cql_filter'];
+    }
     let layer = this.getLayerByLayerName(layerName);
     if (!(layer instanceof ol.layer.Image)) {
       layer = null;
@@ -403,26 +425,7 @@ class Layer {
           crossOrigin: params['crossOrigin']
             ? params['crossOrigin']
             : undefined,
-          params: {
-            LAYERS: params['layers'], // require
-            STYLES: params['styles'] ? params['styles'] : '',
-            VERSION: params['version'] ? params['version'] : '1.3.0',
-            WIDTH: params['width'] ? params['width'] : 256,
-            HEIGHT: params['height'] ? params['height'] : 256,
-            BBOX: params['bbox'], // require
-            SRS: params['srs'] ? params['srs'] : 'EPSG:3857',
-            CRS: params['srs'] ? params['srs'] : 'EPSG:3857',
-            REQUEST: 'GetMap',
-            TRANSPARENT: true,
-            TILED: params['tiled'] === false ? params['tiled'] : true,
-            TILESORIGIN: params['tiledsorrigin']
-              ? params['tiledsorrigin']
-              : undefined,
-            SERVICE: 'WMS',
-            FORMAT: params['format'] ? params['format'] : 'image/png',
-            CQL_FILTER: params['cql_filter'] ? params['cql_filter'] : '',
-            VIEWPARAMS: params['viewparams'] ? params['viewparams'] : ''
-          },
+          params: _config,
           wrapX: false
         })
       });
